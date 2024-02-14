ea9c42937dfbf84eae255b81f16930fe898ce5c0
José San Martin <jose@josesanmartin.net>
2024-02-14 19:04:18 -0300

    Adds session cache for users

--- src/Providers/SessionUserProvider.php
+++ src/Providers/SessionUserProvider.php
@@ -2,7 +2,45 @@
 
 namespace SlashId\Laravel\Providers;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Request;
+use SlashId\Laravel\SlashIdUser;
+
 class SessionUserProvider extends StatelessUserProvider
 {
+
+    public function retrieveById($identifier)
+    {
+        if (!array_key_exists($identifier, $this->localCacheUsers)) {
+            $sessionUser = $this->retrieveSessionUser();
+            if ($sessionUser && $sessionUser->getAuthIdentifier() === $identifier) {
+                $this->localCacheUsers[$identifier] = $sessionUser;
+            }
+            else {
+                $this->localCacheUsers[$identifier] = $this->retrieveByIdFromApi($identifier);
+            }
+        }
+
+        return $this->localCacheUsers[$identifier];
+    }
+
+    public function validateCredentials(Authenticatable $user, array $credentials)
+    {
+        if (parent::validateCredentials($user, $credentials)) {
+            // When the user is authenticated, save it to the session.
+            Request::session()->put('slashid_user', $user);
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+
+    protected function retrieveSessionUser(): ?SlashIdUser {
+        $session = Request::session();
+        if ($session->has('slashid_user')) {
+            return $session->get('slashid_user');
+        }
+        return NULL;
+    }
 
 }
