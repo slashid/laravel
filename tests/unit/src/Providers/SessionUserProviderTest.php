b0bb603f436a44960ae7c375f3dd0bb75f530522
José San Martin <jose@josesanmartin.net>
2024-03-12 17:13:01 -0300

    Fixes coding styles with Pint

--- tests/unit/src/Providers/SessionUserProviderTest.php
+++ tests/unit/src/Providers/SessionUserProviderTest.php
@@ -24,7 +24,7 @@ class SessionUserProviderTest extends SlashIdUserTest
         return [
             [false, false],
             [true,  false],
-            [true,  true ],
+            [true,  true],
         ];
     }
 
@@ -53,7 +53,7 @@ class SessionUserProviderTest extends SlashIdUserTest
             ->expects($hasSession ? $this->once() : $this->never())
             ->method('get')
             ->with($this->identicalTo('slashid_user_9999-9999-9999'))
-            ->willReturn($hasSession ? new SlashIdUser('9999-9999-9999', []) : NULL);
+            ->willReturn($hasSession ? new SlashIdUser('9999-9999-9999', []) : null);
 
         $expectations = $sdk->expects($hasSession ? $this->never() : $this->once())
             ->method('get')
